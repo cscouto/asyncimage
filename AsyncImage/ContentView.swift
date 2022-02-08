bf845fb1fb415ed56a97216cd8806ee23d037a06
Tiago Do Couto <tiago.couto@beautycounter.com>
2022-02-08 17:02:50 -0500

    project

--- AsyncImage/ContentView.swift
+++ AsyncImage/ContentView.swift
@@ -7,15 +7,48 @@
 
 import SwiftUI
 
+extension Image {
+    func modifyToFit() -> some View {
+        self.resizable()
+            .scaledToFit()
+    }
+    
+    func icon() -> some View {
+        self.modifyToFit()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
+    private let imageUrl = "https://credo.academy/credo-academy@3x.png"
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        AsyncImage(url: URL(string: imageUrl), transaction: Transaction(animation: .spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.25))) { phase in
+            switch phase {
+            case .success(let image):
+                image
+                    .modifyToFit()
+                    //.transition(.move(edge: .bottom))
+                    //.transition(.slide)
+                    .transition(.scale)
+            case.failure(_):
+                Image(systemName: "ant.circle.fill").icon()
+            case .empty:
+                Image(systemName: "photo.circle.fill").icon()
+            @unknown default:
+                ProgressView()
+            }
+        }
+        .padding(40)
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
